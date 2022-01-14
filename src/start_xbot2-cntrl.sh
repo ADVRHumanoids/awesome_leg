48c26bb3ddd12e34f8a191acb553425c01b6821f
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-01-14 17:42:21 +0100

    added plotter launch file and modified some xbot config files

--- src/start_xbot2-cntrl.sh
+++ src/start_xbot2-cntrl.sh
@@ -2,4 +2,4 @@
 
 sleep 4 # waiting 4s to give time to gazebo to load (this may differe depending on the machine on which gazebo in run). A smarter alternative should actually be used.
 
-rosservice call /xbotcore/ros_ctrl/switch true # enabling ros_ctrl (to be able to send commands through ROS); this can also be done through the XBot GUI.
+rosservice call /xbotcore/ros_control/switch true # enabling ros_ctrl (to be able to send commands through ROS); this can also be done through the XBot GUI.
--- src/start_xbot2-core.sh
+++ src/start_xbot2-core.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 4 # waiting 4s to give time to gazebo to load
 
-xbot2-core --config /home/andreap/hhcm_ws/src/awesome_leg_pholus/config/xbot2_sim_config.yaml
+xbot2-core --config /home/andreap/hhcm_ws/src/awesome_leg_pholus/config/xbot2_testing_config.yaml
