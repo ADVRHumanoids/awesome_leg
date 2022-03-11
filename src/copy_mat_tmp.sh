f5a4cb451427ffd2eae033fdd7ca3841053a9f96
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-03-11 12:17:51 +0100

    some safety changes

--- src/copy_mat_tmp.sh
+++ src/copy_mat_tmp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-scp embedded@10.24.10.100:/tmp/*.mat ~/hhcm_ws/src/awesome_leg_pholus/test_results/fixed_hip_setup/tip_cart_imp_cntrl/mat_logs
+scp embedded@10.24.10.100:/tmp/*.mat ~/hhcm_ws/src/awesome_leg_pholus/test_results/fixed_hip_setup/tip_cart_imp_cntrl/traj_logs
+# scp embedded@10.24.10.100:/tmp/*.mat ~/hhcm_ws/src/awesome_leg_pholus/test_results/fixed_hip_setup/tip_cart_imp_cntrl/marker_logs
+
+# scp embedded@10.24.10.100:/tmp/*.mat ~/hhcm_ws/src/awesome_leg_pholus/test_results/fixed_hip_setup/tip_cart_acc_cntrl/traj_logs
+# scp embedded@10.24.10.100:/tmp/*.mat ~/hhcm_ws/src/awesome_leg_pholus/test_results/fixed_hip_setup/tip_cart_acc_cntrl/marker_logs
 # scp embedded@10.24.10.100:/tmp/*.mat ~/hhcm_ws/src/awesome_leg_pholus/test_results/fixed_hip_setup/tip_cart_acc_cntrl/mat_logs
