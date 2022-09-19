d120ef49b0646d3cb51ee6c4de3daf5036c9ec97
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-09-19 17:09:00 +0200

    new meshes and inertial paramters integration into URDFs

--- description/urdf/generate_urdfs.sh
+++ description/urdf/generate_urdfs.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-xacro awesome_leg_cartesio.urdf.xacro -o generated/awesome_leg_cartesio.urdf 
-xacro awesome_leg_reduced_calibrated.urdf.xacro -o generated/awesome_leg_reduced_calibrated.urdf 
-xacro awesome_leg_reduced.urdf.xacro -o generated/awesome_leg_reduced.urdf 
+xacro awesome_leg_standalone.urdf.xacro -o generated/awesome_leg_standalone.urdf 
 xacro awesome_leg_test_rig.urdf.xacro -o generated/awesome_leg_test_rig.urdf
-xacro awesome_leg.urdf.xacro -o generated/awesome_leg.urdf
 
  
