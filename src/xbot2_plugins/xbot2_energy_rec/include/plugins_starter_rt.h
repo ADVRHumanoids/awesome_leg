3e98b55122b4768cd2327e8a284e8ac32adf3826
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-01-25 14:58:03 +0100

    first tests with asynchronous internal service

---
+++ src/xbot2_plugins/xbot2_energy_rec/include/plugins_starter_rt.h
@@ -0,0 +1,69 @@
+#ifndef PLUGINS_STARTER_H
+#define PLUGINS_STARTER_H
+
+#include <xbot2/xbot2.h>
+
+
+using namespace XBot;
+
+/**
+ * @brief use of BehaviorTreeCPP_v3 inside of a rt plugin
+ *
+ */
+
+class PluginsStarterRt : public ControlPlugin
+{
+
+public:
+
+    using ControlPlugin::ControlPlugin;
+
+    bool on_initialize() override;
+
+    void starting() override;
+
+    void run() override;
+
+    void stopping() override;
+
+    void on_stop() override;
+
+    void on_close() override;
+
+    void on_abort() override;
+
+    void on_feedback_received(const bool& msg);
+
+private:
+
+    bool _rt_active, _nrt_exit,
+        _is_sim = true,
+        _plugins_started = false;
+
+    int _queue_size = 1,
+        _n_plugins = 0;
+
+    double _plugin_dt;
+
+    std::vector<std::string> _plugin_list;
+    std::vector<bool> _active_plugins;
+
+    std::string _async_service_pattern = "/xbotcore/async_service/xbot_internal/scheduler/";
+
+    // queue object to handle multiple subscribers/servers at once
+    std::vector<CallbackQueue> _queues;
+
+    // internal publisher and subscribers
+    std::vector<PublisherPtr<Runnable::Command>> _req_pubs;
+    std::vector<SubscriberPtr<bool>> _res_subs;
+
+    void read_config_from_yaml();
+
+    void spawn_nrt_thread();
+
+    void init_dump_logger();
+
+
+};
+
+#endif // PLUGINS_STARTER_H
---
+++ src/xbot2_plugins/xbot2_energy_rec/src/plugins_starter_rt.cpp
@@ -0,0 +1,100 @@
+#include "plugins_starter_rt.h"
+
+void PluginsStarterRt::read_config_from_yaml()
+{
+    _plugin_list = getParamOrThrow<std::vector<std::string>>("~plugin_list");
+
+    _queue_size = getParamOrThrow<int>("~queue_size");
+
+    _async_service_pattern = getParamOrThrow<std::string>("~async_service_pattern");
+}
+
+void PluginsStarterRt::on_feedback_received(const bool& msg)
+{
+    _plugins_started = msg;
+
+
+
+}
+
+bool PluginsStarterRt::on_initialize()
+{
+    std::string sim_flagname = "sim";
+
+    read_config_from_yaml(); // load params from yaml file
+
+    _plugin_dt = getPeriodSec();
+
+    _n_plugins = _plugin_list.size();
+
+    _req_pubs = std::vector<PublisherPtr<Runnable::Command>>(_n_plugins);
+    _res_subs = std::vector<SubscriberPtr<bool>>(_n_plugins);
+
+    _active_plugins = std::vector<bool>(_n_plugins);
+
+    for(int i = 0; i < _n_plugins; i++)
+    {
+        auto res_callback = [i, this](const bool& msg)
+        {
+            _active_plugins[i] = msg;
+
+            jhigh().jprint(fmt::fg(fmt::terminal_color::blue),
+                               "Received response from plugin: {} \n",
+                               _plugin_list[i]);
+        };
+
+        _req_pubs[i] = advertise<Runnable::Command>(_async_service_pattern + _plugin_list[i] + "/command/request");
+        _res_subs[i] = subscribe<bool>(_async_service_pattern + _plugin_list[i] + "/command/response",
+                                    res_callback,
+                                    _queue_size);
+
+        _active_plugins[i] = false;
+    }
+
+    return true;
+
+}
+
+void PluginsStarterRt::starting()
+{
+
+    // Move on to run()
+    start_completed();
+
+}
+
+void PluginsStarterRt::run()
+{
+
+    for(int i = 0; i < _n_plugins; i++)
+    {
+        if(!_active_plugins[i])
+        {
+            _req_pubs[i]->publish(Command::Start);
+        }
+
+    }
+
+}
+
+void PluginsStarterRt::on_stop()
+{
+
+}
+
+void PluginsStarterRt::stopping()
+{
+    stop_completed();
+}
+
+void PluginsStarterRt::on_abort()
+{
+
+}
+
+void PluginsStarterRt::on_close()
+{
+    jinfo("Closing PluginsStarterRt");
+}
+
+XBOT2_REGISTER_PLUGIN(PluginsStarterRt, plugins_strt_rt)
