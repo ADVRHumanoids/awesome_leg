221e1a73a0c6f60c299b911329f119c945a52ab6
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2023-02-03 20:20:46 +0100

    added automatic urdf generation upon compilation

--- description/urdf/generate_urdfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xacro awesome_leg_standalone.urdf.xacro -o generated/awesome_leg_standalone.urdf 
-xacro awesome_leg_xbot.urdf.xacro -o generated/awesome_leg_xbot.urdf 
-xacro awesome_leg_test_rig.urdf.xacro -o generated/awesome_leg.urdf
-xacro awesome_leg_test_rig.urdf.xacro sliding_guide:=false -o generated/awesome_leg_no_sliding.urdf
-xacro awesome_leg_floating.urdf.xacro -o generated/awesome_leg_floating.urdf
-
