e8b131f1e5531f940de82066a612618c59cc9b2c
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-02-08 17:51:33 +0100

    implemented restast_jump_sequence

--- src/xbot2_plugins/xbot2_energy_rec/src/actions/restart_jump_sequence.cpp
+++ src/xbot2_plugins/xbot2_energy_rec/src/actions/restart_jump_sequence.cpp
@@ -11,26 +11,11 @@ RestartJumpSequence::RestartJumpSequence(const std::string& name) :
     _name{name}
 {
 
-    setRegistrationID(_name);
-
-    _asynch_servicepath = _async_service_pattern + _idle_state_servername + "/request";
-
-    _set2idle_pub = advertise<awesome_leg::IdleState>(_asynch_servicepath);
-
-    _set2idle.idle = true;
 
 }
 
 NodeStatus RestartJumpSequence::tick()
 {
-
-    _set2idle_pub->publish(_set2idle);
-
-    if(_verbose)
-    {
-        std::cout << Colors::kMagenta << "ticking " + _name + " action" << Colors::kEndl << std::endl;
-
-    }
 
     return NodeStatus::RUNNING;
 
