120288521558d0129d43e6a22e825ec1e267786b
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-02-19 23:01:37 +0100

    minor change

--- src/horizon_code/jump_utils/up2apex.py
+++ src/horizon_code/jump_utils/up2apex.py
@@ -107,8 +107,6 @@ class up2ApexGen:
 
         self.dt_res = self.yaml_file[self.yaml_tag]["resampling"]["dt"] 
         
-        self.f_z_ub = self.yaml_file[self.yaml_tag]["problem"]["f_z_ub"] 
-
         self.use_same_weights = self.yaml_file[self.yaml_tag]["problem"]["weights"]["use_same_weights"]
 
         weight_selector = self.ref_prb_weight_tag if (self.is_ref_prb and not self.use_same_weights) else self.raw_prb_weight_tag
@@ -293,8 +291,8 @@ class up2ApexGen:
         # tip_above_ground = self.prb.createConstraint("tip_above_ground", self.foot_tip_position[2])  # no ground penetration on all the horizon
         # tip_above_ground.setBounds(0.0, cs.inf)
 
-        com_towards_vertical = self.prb.createIntermediateConstraint("com_towards_vertical", self.vcom[2], self.contact_nodes) # intermediate, so all except last node
-        com_towards_vertical.setBounds(0.0, cs.inf)
+        # com_towards_vertical = self.prb.createIntermediateConstraint("com_towards_vertical", self.vcom[2], self.contact_nodes) # intermediate, so all except last node
+        # com_towards_vertical.setBounds(0.0, cs.inf)
 
         com_towards_vertical = self.prb.createConstraint("com_apex", self.vcom[2], \
                     nodes = self.apex_node) # reach the apex at the end of the trajectory 
@@ -304,7 +302,7 @@ class up2ApexGen:
         self.prb.createIntermediateConstraint("grf_zero", self.f_contact, nodes = self.flight_nodes[:-1])  # 0 GRF during flight
 
         grf_positive = self.prb.createIntermediateConstraint("grf_positive", self.f_contact[2], nodes = self.contact_nodes)  # 0 GRF during flight
-        grf_positive.setBounds(0.001, self.f_z_ub)
+        grf_positive.setBounds(0.0, cs.inf)
 
         self.prb.createConstraint("leg_starts_still", self.q_p_dot,
                             nodes=0) # leg starts still
