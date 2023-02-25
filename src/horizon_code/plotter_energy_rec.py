4718e44b79b384198f733d90ef0880baa64c9db0
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-02-25 11:18:47 +0100

    prototype of touchdown optimization

---
+++ src/horizon_code/plotter_energy_rec.py
@@ -0,0 +1,103 @@
+from horizon.utils import kin_dyn
+import numpy as np
+
+from horizon.utils import mat_storer
+
+import matplotlib.pyplot as plt
+# from matplotlib import cm
+# from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+path = '/tmp/touchdown_opt/energy_recov.mat'
+loader = mat_storer.matStorer(path)
+solution_raw = loader.load()
+
+
+n_int = solution_raw["n_int"][0][0]
+q_p_raw = solution_raw["q_p"]  # excluding test rig dof
+q_p_dot_raw = solution_raw["q_p_dot"]
+q_p_ddot_raw = solution_raw["q_p_ddot"]
+# i_q_raw = solution_raw["i_q"]
+GRF_raw = solution_raw["f_contact"]
+tau_raw = solution_raw["tau_sol"]
+dt_raw = solution_raw["dt"].flatten()
+kp_raw = solution_raw['kp']
+kd_raw = solution_raw['kd']
+q_init_raw = solution_raw['q_init']
+
+print(q_init_raw)
+time_vector_raw = np.zeros([n_int + 1])
+for i in range(dt_raw.size):
+    time_vector_raw[i + 1] = time_vector_raw[i] + dt_raw[i]
+    
+f1 = plt.figure()
+plt.plot(time_vector_raw[1:-1], GRF_raw[0, :-1], label=r"F_x")
+plt.plot(time_vector_raw[1:-1], GRF_raw[1, :-1], label=r"F_y")
+plt.plot(time_vector_raw[1:-1], GRF_raw[2, :-1], label=r"F_z")
+legend = plt.legend(loc="upper left")
+legend.set_draggable(state=True)
+plt.xlabel(r"t [s]")
+plt.ylabel('[N]')
+plt.title("Ground reaction forces - raw solution", fontdict=None, loc='center')
+plt.grid()
+# if save_fig:
+#     plt.savefig(save_path+"GRF.pdf", format="pdf")
+
+f2 = plt.figure()
+plt.plot(time_vector_raw[:-1], q_p_raw[0, :-1], label=r"$\mathrm{q}_{\mathrm{base}}$")
+plt.plot(time_vector_raw[:-1], q_p_raw[1, :-1], label=r"$\mathrm{q}_{\mathrm{hip}}$")
+plt.plot(time_vector_raw[:-1], q_p_raw[2, :-1], label=r"$\mathrm{q}_{\mathrm{knee}}$")
+legend = plt.legend(loc="upper left")
+legend.set_draggable(state=True)
+plt.xlabel(r"t [s]")
+plt.ylabel(r"[rad]")
+plt.title("Joint positions - raw solution", fontdict=None, loc='center')
+plt.grid()
+
+f3 = plt.figure()
+plt.plot(time_vector_raw[:-1], q_p_dot_raw[0, :-1], label=r"$\dot{q}_{\mathrm{base}}$")
+plt.plot(time_vector_raw[:-1], q_p_dot_raw[1, :-1], label=r"$\dot{q}_{\mathrm{hip}}$")
+plt.plot(time_vector_raw[:-1], q_p_dot_raw[2, :-1], label=r"$\dot{q}_{\mathrm{knee}}$")
+legend = plt.legend(loc="upper left")
+legend.set_draggable(state=True)
+plt.xlabel(r"t [s]")
+plt.ylabel(r"[rad/s]")
+plt.title("Joint velocities - raw solution", fontdict=None, loc='center')
+plt.grid()
+
+f4 = plt.figure()
+plt.plot(time_vector_raw[:-1], q_p_ddot_raw[0, :],
+         label=r"$\ddot{\mathrm{q}}_{\mathrm{hip}}$")
+plt.plot(time_vector_raw[:-1], q_p_ddot_raw[1, :],
+         label=r"$\ddot{\mathrm{q}}_{\mathrm{knee}}$")
+legend = plt.legend(loc="upper left")
+legend.set_draggable(state=True)
+plt.xlabel(r"t [s]")
+plt.ylabel(r"[$\mathrm{rad}/\mathrm{s}^2$]")
+plt.title("Joint accelerations - raw solution", fontdict=None, loc='center')
+plt.grid()
+
+f5 = plt.figure()
+plt.plot(time_vector_raw[:-1], tau_raw[0, :], drawstyle='steps-post', label=r"$\tau_{\mathrm{base}}$ torque")
+plt.plot(time_vector_raw[:-1], tau_raw[1, :], drawstyle='steps-post', label=r"$\tau_{\mathrm{hip}}$ torque")
+plt.plot(time_vector_raw[:-1], tau_raw[2, :], drawstyle='steps-post', label=r"$\tau_{\mathrm{knee}}$ torque")
+legend = plt.legend(loc="upper left")
+legend.set_draggable(state=True)
+plt.xlabel(r"t [s]")
+plt.ylabel(r"[Nm]")
+plt.title("Joint efforts (link side) - raw solution", fontdict=None, loc='center')
+plt.grid()
+
+plt.show()
+# print(time_vector_raw[:-1].shape)
+# print(dt_raw.shape)
+#
+# f8 = plt.figure()
+# plt.plot(time_vector_raw[:-1], dt_raw, drawstyle='steps-post')
+# legend = plt.legend(loc="upper left")
+# legend.set_draggable(state=True)
+# plt.ylabel(r"t [s]")
+# plt.xlabel("node number")
+# plt.title("Optimization dt - raw solution", fontdict=None, loc='center')
+# plt.grid()
+
--- src/horizon_code/touchdown_opt.py
+++ src/horizon_code/touchdown_opt.py
@@ -4,7 +4,7 @@ import argparse
 
 import rospkg
 
-from jump_utils.pretakeoff import preTakeoffTO
+from jump_utils.energy_rec import landingEnergyRecover
 from jump_utils.up2apex import up2ApexGen
 from jump_utils.full_gen import fullJumpGen
 
@@ -24,169 +24,68 @@ from jump_utils.miscell_utils import str2bool
 file_name = os.path.splitext(os.path.basename(__file__))[0]
 file_name.replace(".py", "")
 
-unique_id = date.today().strftime("%d-%m-%Y") + "-" +\
-                    datetime.now().strftime("%H_%M_%S")
-
-pretakeoff_opt_name = "pretakeoff"
-apex_opt_name = "apex"
-full_opt_name = "full"
+unique_id = date.today().strftime("%d-%m-%Y") + "-" + \
+            datetime.now().strftime("%H_%M_%S")
 
 sol_mat_basename = "awesome_jump"
 
 def main(args):
+    landing_generator = landingEnergyRecover(horizon_config_fullpath,
+                                             actuators_config_fullpath,
+                                             urdf_full_path,
+                                             args.results_dir)
 
-    if args.opt_type == pretakeoff_opt_name:
+    landing_generator.init_prb()
 
-        jump_generator = preTakeoffTO(horizon_config_fullpath, 
-                    actuators_config_fullpath, 
-                    urdf_full_path,
-                    args.results_dir, 
-                    sol_mat_name = pretakeoff_opt_name + "_" + args.sol_mat_name, 
-                    sol_mat_name_res = pretakeoff_opt_name + "_" + args.sol_mat_name_res, 
-                    acc_based_formulation = args.is_acc_based_form)
+    landing_generator.setup_prb()
 
-        jump_generator.init_prb()
+    landing_generator.solve_prb()
 
-        jump_generator.setup_prb()
+    landing_generator.postproc_sol()
 
-        jump_generator.solve_prb()
-
-        jump_generator.postproc_sol()
-
-    if args.opt_type == apex_opt_name:
-        
-        # raw + resampled solution generation
-        jump_generator = up2ApexGen(horizon_config_fullpath, 
-                    actuators_config_fullpath, 
-                    urdf_full_path,
-                    args.results_dir, 
-                    sol_mat_name = apex_opt_name + "_" + args.sol_mat_name, 
-                    sol_mat_name_res = apex_opt_name + "_" + args.sol_mat_name_res, 
-                    sol_mat_name_ref = apex_opt_name + "_" + args.sol_mat_name_ref,
-                    is_ref_prb = False, 
-                    acc_based_formulation = args.is_acc_based_form)
-
-        jump_generator.init_prb()
-        
-        jump_generator.setup_prb()
-
-        jump_generator.solve_prb()
-
-        jump_generator.postproc_sol()
-
-        # refined solution generation
-        jump_generator = up2ApexGen(horizon_config_fullpath, 
-                    actuators_config_fullpath, 
-                    urdf_full_path,
-                    args.results_dir, 
-                    sol_mat_name = apex_opt_name + "_" + args.sol_mat_name, 
-                    sol_mat_name_res = apex_opt_name + "_" + args.sol_mat_name_res, 
-                    sol_mat_name_ref = apex_opt_name + "_" + args.sol_mat_name_ref,
-                    is_ref_prb = True, 
-                    load_ig = args.load_ig, 
-                    acc_based_formulation = args.is_acc_based_form)
-
-        jump_generator.init_prb()
-        
-        jump_generator.setup_prb()
-
-        jump_generator.solve_prb()
-
-        jump_generator.postproc_sol()
-
-    if args.opt_type == full_opt_name:
-
-        # raw + resampled solution generation
-        jump_generator = fullJumpGen(horizon_config_fullpath, 
-                    actuators_config_fullpath, 
-                    urdf_full_path,
-                    args.results_dir, 
-                    sol_mat_name = full_opt_name + "_" + args.sol_mat_name, 
-                    sol_mat_name_res = full_opt_name + "_" + args.sol_mat_name_res, 
-                    sol_mat_name_ref = full_opt_name + "_" + args.sol_mat_name_ref,
-                    is_ref_prb = False, 
-                    acc_based_formulation = args.is_acc_based_form)
-
-        jump_generator.init_prb()
-        
-        jump_generator.setup_prb()
-
-        jump_generator.solve_prb()
-
-        jump_generator.postproc_sol()
-
-        # refined solution generation
-        jump_generator = fullJumpGen(horizon_config_fullpath, 
-                    actuators_config_fullpath, 
-                    urdf_full_path,
-                    args.results_dir, 
-                    sol_mat_name = full_opt_name + "_" + args.sol_mat_name, 
-                    sol_mat_name_res = full_opt_name + "_" + args.sol_mat_name_res, 
-                    sol_mat_name_ref = full_opt_name + "_" + args.sol_mat_name_ref,
-                    is_ref_prb = True, 
-                    load_ig = args.load_ig, 
-                    acc_based_formulation = args.is_acc_based_form)
-
-        jump_generator.init_prb()
-        
-        jump_generator.setup_prb()
-
-        jump_generator.solve_prb()
-
-        jump_generator.postproc_sol()
-
-    
 
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser(
         description='Pipeline script for the generation of offline jumps of the Awesome Leg')
 
     # first level specific arguments
-    parser.add_argument('--urdf_path', '-urdf', type = str, default= rospkg.RosPack().get_path("awesome_leg") + "/description/urdf"+ "/" + "awesome_leg_test_rig" + ".urdf")
-    
-    parser.add_argument('-sol_mat_name', "-matn", type = str, default = sol_mat_basename)
-    parser.add_argument('-sol_mat_name_res', "-matn_res", type = str, default = sol_mat_basename + "_res")
-    parser.add_argument('-sol_mat_name_ref', "-matn_ref", type = str, default = sol_mat_basename + "_ref")
+    parser.add_argument('--urdf_path', '-urdf', type=str, default=rospkg.RosPack().get_path(
+        "awesome_leg") + "/description/urdf" + "/" + "awesome_leg_test_rig" + ".urdf")
 
-    parser.add_argument('--results_dir', '-rdir', type = str, help = 'where results are saved', default = "/tmp/" + file_name + "_" + unique_id)
+    parser.add_argument('-sol_mat_name', "-matn", type=str, default=sol_mat_basename)
 
-    parser.add_argument('--hor_confname', '-hconf', type = str,\
-                        help = 'horizon config file name', default = file_name)
-                        
-    parser.add_argument('--opt_type', '-optt', type = str,\
-                        help = 'optimization type', default = apex_opt_name, 
-                        choices = [pretakeoff_opt_name, apex_opt_name, full_opt_name])
+    parser.add_argument('--results_dir', '-rdir', type=str, help='where results are saved',
+                        default="/tmp/" + file_name) #+ "_" + unique_id
 
-    parser.add_argument('--is_acc_based_form', '-isacc', type = str2bool,\
-                        help = 'whether to use an acceleration-based formulation or not', default = True)
-
-    parser.add_argument('--load_ig', '-uig', type = str2bool, default = True)
+    parser.add_argument('--hor_confname', '-hconf', type=str,
+                        help='horizon config file name', default=file_name)
 
     args = parser.parse_args()
 
-    rospackage = rospkg.RosPack() # Only for taking the path to the leg package
+    rospackage = rospkg.RosPack()  # Only for taking the path to the leg package
     exec_path = rospackage.get_path("awesome_leg") + "/src/horizon_code"
     urdfs_path = rospackage.get_path("awesome_leg") + "/description/urdf"
     urdf_name = "awesome_leg_test_rig"
     urdf_full_path = urdfs_path + "/" + urdf_name + ".urdf"
     xacro_full_path = urdfs_path + "/" + urdf_name + ".urdf.xacro"
-    
+
     sliding_guide_command = "sliding_guide:=" + "true"
 
-    config_path=rospackage.get_path("awesome_leg")+"/config/" # configuration files path
-    horizon_config_path = config_path + "horizon/jump_generation/"
+
+    config_path = rospackage.get_path("awesome_leg") + "/config/"  # configuration files path
+    horizon_config_path = config_path + "horizon/touchdown_opt/"
     horizon_config_fullpath = horizon_config_path + args.hor_confname + ".yaml"
     actuators_config_fullpath = config_path + "actuators.yaml"
 
     try:
 
         print(colored("\n--> GENERATING LEG URDF...\n", "blue"))
-        xacro_gen = subprocess.check_call(["xacro",\
-                                        xacro_full_path, \
-                                        sliding_guide_command, \
-                                        "-o", 
-                                        urdf_full_path])
+        xacro_gen = subprocess.check_call(["xacro",
+                                           xacro_full_path,
+                                           sliding_guide_command,
+                                           "-o",
+                                           urdf_full_path])
 
         print(colored("\n--> URDF GENERATED SUCCESSFULLY. \n", "blue"))
 
@@ -194,10 +93,11 @@ if __name__ == '__main__':
 
         print(colored('FAILED TO GENERATE URDF.', "red"))
 
-    os.mkdir(args.results_dir)
-    shutil.copyfile(actuators_config_fullpath, args.results_dir + "/actuators" + unique_id + ".yaml") 
-    shutil.copyfile(horizon_config_fullpath, args.results_dir + "/" + "horizon_config.yaml" )
-    shutil.copyfile(xacro_full_path, args.results_dir + "/" + urdf_name + ".urdf.xacro" )
-    shutil.copyfile(urdf_full_path, args.results_dir + "/" + urdf_name + ".urdf" )
-    
+    if not os.path.isdir(args.results_dir):
+        os.mkdir(args.results_dir)
+    shutil.copyfile(horizon_config_fullpath, args.results_dir + "/" + "touchdown_opt.yaml")
+    shutil.copyfile(actuators_config_fullpath, args.results_dir + "/actuators" + unique_id + ".yaml")
+    shutil.copyfile(xacro_full_path, args.results_dir + "/" + urdf_name + ".urdf.xacro")
+    shutil.copyfile(urdf_full_path, args.results_dir + "/" + urdf_name + ".urdf")
+
     main(args)
