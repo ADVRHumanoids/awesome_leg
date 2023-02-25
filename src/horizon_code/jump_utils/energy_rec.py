cb608eec6f38592157c22625c1b88b4f26229a22
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-02-25 15:11:17 +0100

    almost working touchdown optimization

--- src/horizon_code/jump_utils/energy_rec.py
+++ src/horizon_code/jump_utils/energy_rec.py
@@ -53,7 +53,7 @@ class landingEnergyRecover:
         self.__read_opts_from_yaml()
 
     def __read_opts_from_yaml(self):
-
+        
         yaml_info = self.yaml_file[self.yaml_tag]
 
         self.n_int = yaml_info['problem']['n_int']
@@ -70,26 +70,20 @@ class landingEnergyRecover:
         self.trans_name = yaml_info["transcription"]["name"]
         self.trans_integrator = yaml_info["transcription"]["integrator_name"]
 
-        self.weight_jnt_input_diff = yaml_info_weights['weight_jnt_input_diff']
-        self.weight_f_contact_diff = yaml_info_weights["weight_f_contact_diff"]
         self.weight_f_contact_cost = yaml_info_weights["weight_f_contact"]
-        self.weight_q_dot = yaml_info_weights["weight_q_p_dot"]
-        # self.weight_q_ddot         = yaml_info_weights["weight_q_p_ddot"]
-        # self.weight_q_p_ddot_diff  = yaml_info_weights["weight_jnt_input_diff"]
+        self.weight_f_contact_diff = yaml_info_weights["weight_f_contact_diff"]
+        self.weight_jnt_input = yaml_info_weights['weight_jnt_input']
+        self.weight_jnt_input_diff = yaml_info_weights['weight_jnt_input_diff']
+        self.weight_q_dot = yaml_info_weights["weight_q_dot"]
+        self.weight_q_dot_diff  = yaml_info_weights["weight_q_dot_diff"]
 
-        # self.weight_term_com_vel   = yaml_info_weights["weight_com_term_vel"]
-        # self.weight_com_vel        = yaml_info_weights["weight_com_vel"]
-        # self.weight_tip_under_hip  = yaml_info_weights["weight_tip_under_hip"]
-        # self.weight_sat_i_q        = yaml_info_weights["weight_sat_i_q"]
+        self.weight_reg_energy  = yaml_info_weights["weight_reg_energy"]
+        self.weight_impact_min  = yaml_info_weights["weight_impact_min"]
 
-        # self.weight_com_pos        = yaml_info_weights["weight_com_pos"]
-
-        # self.n_actuators = len(self.act_yaml_file["K_d0"])
-        # self.is_iq_cnstrnt = self.yaml_file[self.yaml_tag]["problem"]["is_iq_cnstrnt"]
-        # self.sliding_guide_kd = self.yaml_file[self.yaml_tag]["sliding_guide_friction"]["kd"]
-        # self.is_sliding_guide_friction = self.yaml_file[self.yaml_tag]["problem"]["is_sliding_guide_friction"]
-        # self.is_friction_cone = self.yaml_file[self.yaml_tag]["problem"]["is_friction_cone"]
-        # self.mu_friction_cone = abs(self.yaml_file[self.yaml_tag]["problem"]["friction_cnstrnt"]["mu_friction_cone"])
+        self.n_actuators = len(self.act_yaml_file["K_d0"])
+        self.is_iq_cnstrnt = self.yaml_file[self.yaml_tag]["problem"]["is_iq_cnstrnt"]
+        self.is_friction_cone = self.yaml_file[self.yaml_tag]["problem"]["is_friction_cone"]
+        self.mu_friction_cone = abs(self.yaml_file[self.yaml_tag]["problem"]["friction_cnstrnt"]["mu_friction_cone"])
 
         self.dt_lb = yaml_info["problem"]["dt_lb"]
         self.dt_ub = yaml_info["problem"]["dt_ub"]
@@ -97,11 +91,77 @@ class landingEnergyRecover:
         self.n_nodes = self.n_int + 1
         self.last_node = self.n_nodes - 1
         self.input_nodes = list(range(0, self.n_nodes - 1))
+        self.input_diff_nodes = list(range(1, self.n_nodes - 1))
 
     def __init_sol_dumpers(self):
 
         self.ms_sol = mat_storer.matStorer(self.results_path + "/" + self.sol_mat_name + ".mat")  # original opt. sol
 
+    def __init_prb(self, n_passive_joints=1):
+
+        self.urdf = open(self.urdf_path, "r").read()
+        self.urdf_kin_dyn = casadi_kin_dyn.py3casadi_kin_dyn.CasadiKinDyn(self.urdf)
+
+        self.n_q = self.urdf_kin_dyn.nq()  # number of joints
+        self.n_v = self.urdf_kin_dyn.nv()  # number of dofs
+
+        # self.I_lim = self.act_yaml_file["I_peak"]  # i_q current limits
+
+        v_bounds = np.array(self.act_yaml_file["omega_max_nl_af44"])
+
+        self.lbs = self.urdf_kin_dyn.q_min()
+        self.ubs = self.urdf_kin_dyn.q_max()
+
+        self.tau_lim = np.array([0] + self.act_yaml_file["tau_peak_ar"])  # effort limits (0 on the passive d.o.f.)
+
+        self.prb = Problem(self.n_int)  # initialization of a problem object
+
+        dt_single_var = self.prb.createSingleVariable("dt", 1)  # dt before the takeoff
+        dt_single_var.setBounds(self.dt_lb, self.dt_ub)  # bounds on dt
+        # dt = 0.01
+        dt = [dt_single_var] * (self.n_int)  # holds the complete time list
+
+        self.prb.setDt(dt)
+
+        # Creating the state variables
+        self.q_p = self.prb.createStateVariable("q_p", self.n_q)
+        self.q_p_dot = self.prb.createStateVariable("q_p_dot",
+                                                    self.n_v)  # here q_p_dot is actually not the derivative of the lagrangian state vector
+        # self.q_p.setBounds(self.lbs, self.ubs)
+        self.q_p_dot[1:3].setBounds(- v_bounds, v_bounds)
+
+        # v0 = np.zeros(self.n_v)
+        # self.q_p_dot.setBounds(v0, v0, nodes=0)
+
+        # Defining the input/s (joint accelerations)
+        self.q_p_ddot = self.prb.createInputVariable("q_p_ddot", self.n_v)
+        self.xdot = utils.double_integrator(self.q_p, self.q_p_dot, self.q_p_ddot)  # building the full state
+
+        # Creating an additional input variable for the contact forces on the foot tip
+        self.f_contact = self.prb.createInputVariable("f_contact", 3)  # dimension 3, no contact torques
+
+        self.kp = self.prb.createSingleVariable('kp', self.n_v - 1) # joint impedance gains (only actuated joints)
+        self.kd = self.prb.createSingleVariable('kd', self.n_v - 1)
+        kp_min = 0 * np.ones(self.n_v - 1)
+        kp_max = 4000 * np.ones(self.n_v - 1)
+        kd_min = 0 * np.ones(self.n_v - 1)
+        kd_max = 100 * np.ones(self.n_v - 1)
+        self.kp.setBounds(kp_min, kp_max)
+        self.kd.setBounds(kd_min, kd_max)
+
+        self.q_landing = self.prb.createSingleVariable('q_landing', self.n_q - 1)
+    
+        self.v_ee_param = self.prb.createParameter('v_ee_param', 1) # vertical vel component of tip @ touchdown
+
+        self.q_p_dot_pretakeoff = cs.veccat(- self.v_ee_param, 0.0, 0.0)
+
+        self.delta_chi_impact = cs.veccat(0.0, 0.0, 0.0 - self.v_ee_param, 0.0, 0.0, 0.0) # total "task" twist vector [[linear], [angular]] @ touchdown
+
+        self.impact = self.prb.createVariable('impact', 6, nodes=0) # impact --> lim_{dt->0}{int_{0}^{dt}{f * d_tau}}
+        #(vertical impact for simplicity)
+
+        return self.prb
+    
     def __get_quantities_from_urdf(self):
 
         # hip link pos
@@ -123,11 +183,12 @@ class landingEnergyRecover:
         self.foot_tip_position = self.fk_foot(q=self.q_p)["ee_pos"]  # foot position
 
         #  tip jacobian
-        self.jac_foot_tip = self.urdf_kin_dyn.jacobian("tip1",
+        self.fk_foot = self.urdf_kin_dyn.jacobian("tip1",
                                                        casadi_kin_dyn.py3casadi_kin_dyn.CasadiKinDyn.LOCAL_WORLD_ALIGNED)
+        self.J_tip = self.fk_foot(q=self.q_p)['J']
 
-        self.b_q_p = self.urdf_kin_dyn.crba()(q=self.q_p)['B']
-
+        self.B = self.urdf_kin_dyn.crba()(q=self.q_p)['B']
+        self.B_q_p_dot = self.B @ self.q_p_dot # we assume q_p_dot pre impact = 0
 
         # foot tip vel
         self.dfk_foot = self.urdf_kin_dyn.frameVelocity("tip1", \
@@ -139,87 +200,78 @@ class landingEnergyRecover:
         self.com = self.com_fk(q=self.q_p, v=self.q_p_dot, a=self.q_p_ddot)["com"]
         self.vcom = self.com_fk(q=self.q_p, v=self.q_p_dot, a=self.q_p_ddot)["vcom"]
 
-    def __set_constraints(self):
+        # impact torques
 
-        # constraints
+        self.JT_i = self.J_tip.T @ self.impact
+
+        self.J_delta_v = self.J_tip @ (self.q_p_dot - self.q_p_dot_pretakeoff)
+
+        # variation of kin energy due to impact
+
+        self.delta_ek = 1/2 * self.impact.T @ self.delta_chi_impact # this is Ek_pst_takeoff - Ek_pre_takeoff <= 0 always for 
+        # energy conservation 
+
+    def __set_constraints(self):
 
         self.tau_limits = self.prb.createIntermediateConstraint("tau_limits", self.tau)  # torque limits
         self.tau_limits.setBounds(- self.tau_lim, self.tau_lim)  # setting input limits
 
-        self.prb.createConstraint("foot_vel_zero", self.v_foot_tip)
-        self.prb.createConstraint("tip_starts_on_ground", self.foot_tip_position[2], nodes=0)
+        self.prb.createConstraint("foot_vel_zero", self.v_foot_tip) # we always have contact
+        self.prb.createConstraint("tip_starts_on_ground", self.foot_tip_position[2], nodes=0) # tip starts on ground
 
-        for dim_i in range(1, self.n_v):
-            self.prb.createIntermediateConstraint(f'stiffness_damping_dim_{dim_i}', self.tau[dim_i] - (
-                        self.kp[dim_i] * (self.q_p[dim_i] - self.q_init[dim_i]) - self.kd[dim_i] * self.q_p_dot[dim_i]))
+        for i in range(0, self.n_v - 1):
+            self.prb.createIntermediateConstraint(f'imp_cntrl_jnt{i}', self.tau[i + 1] - (
+                        self.kp[i] * (self.q_p[i + 1] - self.q_landing[i]) - self.kd[i] * self.q_p_dot[i + 1]))
 
-        self.prb.createConstraint('q_p_impact_ref', self.q_p - self.q_init, nodes=0)
+        self.prb.createConstraint('q_p_at_touchdown', self.q_p[1:] - self.q_landing, nodes=0)
 
-        self.prb.createConstraint('initial_vel', self.v_impact - (self.jac_foot_tip(q=self.q_init)['J'] @ self.q_p_dot), nodes=0)
+        for i in range(0, self.n_v):
+            self.prb.createConstraint('impact_dyn_jnt' + str(i), self.B_q_p_dot[i] - self.JT_i[i], nodes=0) # impact law
 
-        for dim_i in range(1, self.n_v):
-            self.prb.createConstraint(f'impact_{dim_i}', (self.b_q_p @ self.q_p_dot)[dim_i, :] - self.impact[dim_i], nodes=0)
+        for i in range(0, 6):
 
+            self.prb.createConstraint(f'post_impact_jnt_vel_{i}', self.delta_chi_impact[i] - self.J_delta_v[i], nodes=0)
 
-        # self.prb.createConstraint("tip_stays_on_ground", self.v_foot_tip, self.contact_nodes)
+        grf_positive = self.prb.createIntermediateConstraint("grf_positive", self.f_contact[2])
+        grf_positive.setBounds(0.0001, cs.inf)
+        
+        no_energy_violation = self.prb.createConstraint("we_cannot_create_energy_at_touchdown", self.delta_ek, nodes = 0)
+        no_energy_violation.setBounds(-cs.inf, 0)
 
-        # hip_above_ground = self.prb.createConstraint("hip_above_ground", self.hip_position[2])  # no ground penetration on all the horizon
-        # hip_above_ground.setBounds(0.0, cs.inf)
-
-        # tip_above_ground = self.prb.createConstraint("tip_above_ground", self.foot_tip_position[2])  # no ground penetration on all the horizon
-        # tip_above_ground.setBounds(0.0, cs.inf)
-
-        # com_towards_vertical = self.prb.createIntermediateConstraint("com_towards_vertical", self.vcom[2], self.contact_nodes) # intermediate, so all except last node
-        # com_towards_vertical.setBounds(0.0, cs.inf)
-
-        # self.prb.createIntermediateConstraint("grf_zero", self.f_contact, nodes = self.flight_nodes[:-1])  # 0 GRF during flight
-
-        # grf_positive = self.prb.createIntermediateConstraint("grf_positive", self.f_contact[2], nodes = self.contact_nodes)  # 0 GRF during flight
-        # grf_positive.setBounds(0.0, cs.inf)
-
-        # self.prb.createConstraint("leg_starts_still", self.q_p_dot, nodes=0) # leg starts still
+        self.prb.createFinalConstraint("braking_q_dot", self.q_p_dot)
+        self.prb.createConstraint("braking_q_ddot", self.q_p_ddot, nodes = self.last_node -1 )
 
     def __set_costs(self):
 
-        self.prb.createIntermediateCost("min_f_contact", \
-                                        self.weight_f_contact_cost * cs.sumsqr(self.f_contact[0:2]),
-                                        nodes=self.input_nodes)
+        if self.weight_impact_min > 0:
 
-        self.prb.createCost("min_q_dot", self.weight_q_dot * cs.sumsqr(self.q_p_dot))
+            self.prb.createCost("min_impact_residual_kin_energy_dissipation", - self.weight_impact_min * self.delta_ek, nodes = 0) # delta_ek is always <= 0
 
-        self.prb.createCost("min_kin", 1/2 * self.impact.T @ self.v_impact)
+        # regularizations
+        if self.weight_f_contact_cost > 0:
+            self.prb.createIntermediateCost("min_f_contact", \
+                self.weight_f_contact_cost * cs.sumsqr(self.f_contact[0:2]), nodes = self.input_nodes)
 
+        if self.weight_f_contact_diff > 0:
+            self.prb.createIntermediateCost("min_f_contact_diff",\
+                self.weight_f_contact_diff * cs.sumsqr(self.f_contact - self.f_contact.getVarOffset(-1)), nodes = self.input_diff_nodes)
+            
+        if self.weight_q_dot > 0:
+            self.prb.createCost("min_q_dot", self.weight_q_dot * cs.sumsqr(self.q_p_dot), nodes = range(1, self.last_node)) 
 
+        if self.weight_q_dot_diff > 0:
+            self.prb.createIntermediateCost("min_q_dot_diff", self.weight_q_dot_diff * cs.sumsqr(self.q_p_dot - self.q_p_dot.getVarOffset(-1)))
 
+        if self.weight_jnt_input > 0:
+            self.prb.createIntermediateCost("min_q_ddot", self.weight_jnt_input * cs.sumsqr(self.q_p_ddot[1:]), nodes = self.input_nodes) 
 
-        # if self.weight_q_dot_diff > 0:
-        #     self.prb.createIntermediateCost("min_q_dot_diff", self.weight_q_dot_diff * cs.sumsqr(self.q_p_dot - self.q_p_dot.getVarOffset(-1)))
+        if self.weight_jnt_input_diff > 0:
+       
+            jnt_input_diff = cs.sumsqr(self.q_p_ddot[1:] - self.q_p_ddot[1:].getVarOffset(-1))
 
-        # if self.weight_jnt_input > 0:
-        #     self.prb.createIntermediateCost("min_q_ddot", self.weight_jnt_input * cs.sumsqr(self.q_p_ddot[1:]), nodes = self.input_nodes)
-
-        # if self.weight_com_vel > 0:
-        #     self.prb.createIntermediateCost("max_com_vel", self.weight_com_vel * 1/ ( cs.sumsqr(self.vcom[2]) + 0.0001 ))
-
-        # if self.weight_term_com_vel > 0:
-        #     self.prb.createIntermediateCost("max_com_term_vel", self.weight_term_com_vel * 1/ ( cs.sumsqr(self.vcom[2]) + 0.0001 ), \
-        #                                     nodes = self.contact_nodes[-1])
-
-        #
-        # if self.weight_jnt_input_diff > 0:
-        #     jnt_input_diff = cs.sumsqr(self.tau[1:] - self.tau[1:].getVarOffset(-1))
-
-        # self.prb.createIntermediateCost("min_jnt_input_diff", \
-        #     self.weight_jnt_input_diff * jnt_input_diff, nodes = self.input_diff_nodes)
-
-        # if self.weight_f_contact_diff > 0:
-        #     self.prb.createIntermediateCost("min_f_contact_diff",\
-        #         self.weight_f_contact_diff * cs.sumsqr(self.f_contact - self.f_contact.getVarOffset(-1)), nodes = self.input_diff_nodes)
-
-        # if self.weight_tip_under_hip > 0:
-        #     self.prb.createIntermediateCost("max_tip_under_hip", \
-        #         self.weight_tip_under_hip * (cs.sumsqr(self.hip_position[1] - self.foot_tip_position[1])))
-
+            self.prb.createIntermediateCost("min_jnt_input_diff", \
+                self.weight_jnt_input_diff * jnt_input_diff, nodes = self.input_diff_nodes)  
+            
     def __get_solution(self):
 
         self.solution = self.slvr.getSolutionDict()  # extracting solution
@@ -241,66 +293,6 @@ class landingEnergyRecover:
 
         self.ms_sol.store(self.sol_dict_full_raw_sol)  # saving solution data to file
 
-    def __init_prb(self, n_passive_joints=1):
-
-        self.urdf = open(self.urdf_path, "r").read()
-        self.urdf_kin_dyn = casadi_kin_dyn.py3casadi_kin_dyn.CasadiKinDyn(self.urdf)
-
-        self.n_q = self.urdf_kin_dyn.nq()  # number of joints
-        self.n_v = self.urdf_kin_dyn.nv()  # number of dofs
-
-        # self.I_lim = self.act_yaml_file["I_peak"]  # i_q current limits
-
-        v_bounds = np.array(self.act_yaml_file["omega_max_nl_af44"])
-
-        self.lbs = self.urdf_kin_dyn.q_min()
-        self.ubs = self.urdf_kin_dyn.q_max()
-
-        self.tau_lim = np.array([0] + self.act_yaml_file["tau_peak_ar"])  # effort limits (0 on the passive d.o.f.)
-
-        self.prb = Problem(self.n_int)  # initialization of a problem object
-
-        # dt_sing_var = self.prb.createSingleVariable("dt", 1)  # dt before the takeoff
-        # dt_sing_var.setBounds(self.dt_lb, self.dt_ub)  # bounds on dt3
-
-        dt = 0.01
-        # dt = [dt_sing_var] * (self.n_int)  # holds the complete time list
-
-        self.prb.setDt(dt)
-
-        # Creating the state variables
-        self.q_p = self.prb.createStateVariable("q_p", self.n_q)
-        self.q_p_dot = self.prb.createStateVariable("q_p_dot",
-                                                    self.n_v)  # here q_p_dot is actually not the derivative of the lagrangian state vector
-        # self.q_p.setBounds(self.lbs, self.ubs)
-        self.q_p_dot[1:3].setBounds(- v_bounds, v_bounds)
-
-        v0 = np.zeros(self.n_v)
-        self.q_p_dot.setBounds(v0, v0, nodes=0)
-
-        # Defining the input/s (joint accelerations)
-        self.q_p_ddot = self.prb.createInputVariable("q_p_ddot", self.n_v)
-        self.xdot = utils.double_integrator(self.q_p, self.q_p_dot, self.q_p_ddot)  # building the full state
-
-        # Creating an additional input variable for the contact forces on the foot tip
-        self.f_contact = self.prb.createInputVariable("f_contact", 3)  # dimension 3
-
-        self.kp = self.prb.createSingleVariable('kp', self.n_v)
-        self.kd = self.prb.createSingleVariable('kd', self.n_v)
-
-        self.q_init = self.prb.createSingleVariable('q_init', self.n_q)
-
-        self.v_ee_param = self.prb.createParameter('v_ee_param', 1)
-        self.impact = self.prb.createVariable('impact', 6, nodes=0)
-
-        self.v_impact = cs.veccat(0., 0., - self.v_ee_param, 0., 0., 0.)
-
-        # v0_min = 0 * np.ones(self.n_v)
-        # v0_max = 100 * np.ones(self.n_v)
-        # self.kp.setBounds(v0_min, v0_max)
-        # self.kd.setBounds(v0_min, v0_max)
-
-        return self.prb
 
     def init_prb(self):
 
--- src/horizon_code/touchdown_opt.py
+++ src/horizon_code/touchdown_opt.py
@@ -30,6 +30,7 @@ unique_id = date.today().strftime("%d-%m-%Y") + "-" + \
 sol_mat_basename = "awesome_jump"
 
 def main(args):
+    
     landing_generator = landingEnergyRecover(horizon_config_fullpath,
                                              actuators_config_fullpath,
                                              urdf_full_path,
@@ -56,7 +57,7 @@ if __name__ == '__main__':
     parser.add_argument('-sol_mat_name', "-matn", type=str, default=sol_mat_basename)
 
     parser.add_argument('--results_dir', '-rdir', type=str, help='where results are saved',
-                        default="/tmp/" + file_name) #+ "_" + unique_id
+                        default="/tmp/" + file_name + "_" + unique_id) #
 
     parser.add_argument('--hor_confname', '-hconf', type=str,
                         help='horizon config file name', default=file_name)
@@ -71,7 +72,6 @@ if __name__ == '__main__':
     xacro_full_path = urdfs_path + "/" + urdf_name + ".urdf.xacro"
 
     sliding_guide_command = "sliding_guide:=" + "true"
-
 
     config_path = rospackage.get_path("awesome_leg") + "/config/"  # configuration files path
     horizon_config_path = config_path + "horizon/touchdown_opt/"
