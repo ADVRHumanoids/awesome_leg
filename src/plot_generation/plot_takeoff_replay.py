3f26fc68951a11c4edcb22cc2742c31dc9062f3f
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-07-05 16:41:51 +0200

    added boxplot for torques and iq during jumping

--- src/plot_generation/plot_takeoff_replay.py
+++ src/plot_generation/plot_takeoff_replay.py
@@ -91,6 +91,10 @@ class LoadJumpTrial:
 
             self.data[name] = np.array(self.mat_file_h5py[name]).T  # add the pair key-value to the dictionary
 
+        if 'iq_est' in name: # assigns the aux names and their associated codes to a dictionary
+
+            self.data[name] = np.array(self.mat_file_h5py[name]).T  # add the pair key-value to the dictionary
+
         return None # any other value != to None would block the execution of visit() method
 
 class LoadOptJump:
@@ -194,7 +198,9 @@ for i in range(n_samples_replay):
 # print(power_data_log)
 # exit()
 iq_meas = power_data_log.data["iq_meas"]
+iq_est = power_data_log.data["iq_est"]
 q_dot_meas = replay_data_log.data["q_p_dot_cmd"]
+tau_meas_pow_log = power_data_log.data["tau_meas"]
 tau_meas = replay_data_log.data["tau_meas"]
 est_e_rec = power_data_log.data["est_recov_energy_tot"]
 meas_e_rec = power_data_log.data["reg_energy"]
@@ -202,54 +208,89 @@ meas_e_rec = power_data_log.data["reg_energy"]
 selected_time_replay = time_vect_replay[data_selector_replay] - time_vect_replay[data_selector_replay][0]
 selected_time_pow = time_vect_pow[data_selector_pow] - time_vect_pow[data_selector_pow][0]
 
+selected_iq_est = iq_est[:, data_selector_pow]
 selected_iq_meas = iq_meas[:, data_selector_pow]
 selected_q_dot_meas = q_dot_meas[:, data_selector_replay]
 selected_tau_meas = tau_meas[:, data_selector_replay]
+selected_tau_meas_pow = tau_meas_pow_log[:, data_selector_pow]
 selected_est_e_rec = est_e_rec[ data_selector_pow]
 selected_meas_e_rec = meas_e_rec[data_selector_pow]
 
+## Quadrature current - joint efforts -> takeoff replay on real robot ##
+
 fontsize = 14
-f1, ax = plt.subplots(3)
+f1, ax = plt.subplots(2)
 # f1.title()
-ax[0].plot(selected_time_replay, selected_q_dot_meas[0, :], drawstyle='steps-post')
-ax[0].plot(selected_time_replay, selected_q_dot_meas[1, :], drawstyle='steps-post')
+# ax[0].plot(selected_time_replay, selected_q_dot_meas[0, :], drawstyle='steps-post')
+# ax[0].plot(selected_time_replay, selected_q_dot_meas[1, :], drawstyle='steps-post')
+# ax[0].set_ylabel('[rad/s]', fontsize=fontsize)
+# ax[0].set_title('Joint velocities - hardware test', fontsize=fontsize)
+# ax[0].grid()
+# legend = ax[0].legend([r"$v_{1}$", r"$v_{2}$"], fontsize=fontsize)
+# legend.set_draggable(state = True)
 
-# ax1.plot(selected_time_replay, selected_iq_meas[0, :], drawstyle='steps-post')
-# ax[0].set_xlabel('time',  fontsize=fontsize)
-ax[0].set_ylabel('[rad/s]', fontsize=fontsize)
-ax[0].set_title('Joint velocities - hardware test', fontsize=fontsize)
+ax[0].plot(selected_time_pow, selected_iq_est[0, :], drawstyle='steps-post')
+ax[0].plot(selected_time_pow, selected_iq_meas[1, :], drawstyle='steps-post')
+ax[0].set_ylabel('[A]', fontsize=fontsize)
+ax[0].set_title('Quadrature currents - hardware test', fontsize=fontsize)
 ax[0].grid()
-legend = ax[0].legend([r"$v_{1}$", r"$v_{2}$"], fontsize=fontsize)
+legend = ax[0].legend([r"$i_{q0}$", r"$i_{q1}$"], fontsize=fontsize)
 legend.set_draggable(state = True)
 
-ax[1].plot(selected_time_pow, selected_iq_meas[0, :], drawstyle='steps-post')
-ax[1].plot(selected_time_pow, selected_iq_meas[1, :], drawstyle='steps-post')
-# ax1.plot(selected_time_replay, selected_iq_meas[0, :], drawstyle='steps-post')
-# ax[1].set_xlabel('time',  fontsize=fontsize)
-ax[1].set_ylabel('[A]', fontsize=fontsize)
-ax[1].set_title('Quadrature currents - hardware test', fontsize=fontsize)
+ax[1].plot(selected_time_replay, selected_tau_meas[0, :], drawstyle='steps-post')
+ax[1].plot(selected_time_replay, selected_tau_meas[1, :], drawstyle='steps-post')
+ax[1].set_xlabel('time [s]',  fontsize=fontsize)
+ax[1].set_ylabel('[Nm]', fontsize=fontsize)
+ax[1].set_title('Joint efforts - hardware test', fontsize=fontsize)
 ax[1].grid()
-legend = ax[1].legend([r"$i_{q0}$", r"$i_{q1}$"], fontsize=fontsize)
+legend = ax[1].legend([r"$\tau_{1}$", r"$\tau_{2}$"], fontsize=fontsize)
 legend.set_draggable(state = True)
-ax[2].plot(selected_time_replay, selected_tau_meas[0, :], drawstyle='steps-post')
-ax[2].plot(selected_time_replay, selected_tau_meas[1, :], drawstyle='steps-post')
-# ax1.plot(selected_time_replay, selected_iq_meas[0, :], drawstyle='steps-post')
-ax[2].set_xlabel('time [s]',  fontsize=fontsize)
-ax[2].set_ylabel('[Nm]', fontsize=fontsize)
-ax[2].set_title('Joint efforts - hardware test', fontsize=fontsize)
-ax[2].grid()
-legend = ax[2].legend([r"$\tau_{1}$", r"$\tau_{2}$"], fontsize=fontsize)
-legend.set_draggable(state = True)
+
 f1.set_figheight(5)
 f1.set_figwidth(10)
 
-f32, ax32 = plt.subplots(3)
+## Optimized trajectory replay on real robot plots ##
+
+fontsize = 14
+f2, ax2 = plt.subplots(2)
+
+ax2[0].plot(selected_time_pow, selected_iq_meas[0, :], drawstyle='steps-post')
+ax2[0].plot(selected_time_pow, selected_iq_est[0, :], drawstyle='steps-post')
+ax2[0].set_ylabel('[A]', fontsize=fontsize)
+ax2[0].set_title('iq model tracking - hip', fontsize=fontsize)
+ax2[0].grid()
+legend = ax2[0].legend(["estimated" , "meas."], fontsize=fontsize)
+legend.set_draggable(state = True)
+
+combined_data = np.stack((selected_iq_meas[0, :], 
+                        selected_iq_meas[1, :], 
+                        selected_tau_meas_pow[0, :], 
+                        selected_tau_meas_pow[1, :]), axis = 1)
+
+green_diamond = dict(markerfacecolor='g', marker='D')
+artist= ax2[1].boxplot(combined_data,
+            flierprops = green_diamond, vert=True, 
+            whis = 1,
+            labels = [r"$i_q$" + " hip", r"$i_q$" + " knee", r"$\tau$" + " hip", r"$\tau$" + " knee"],
+            autorange = True, 
+            showfliers=True, 
+            notch=False)
+leg2 = ax2[1].legend(artist["boxes"],  [], loc='upper right', handlelength=0, handletextpad=0, fancybox=True, fontsize = fontsize)           
+leg2.set_draggable(state = True)
+ax2[1].set_ylabel(r"[A]" + " - " +  r"[Nm]", fontsize = fontsize)
+ax2[1].set_xlabel("", fontsize = fontsize)
+ax2[1].set_title(r"Take-off $i_q$ and $\tau$", fontdict=None, loc='center', fontsize = fontsize)
+ax2[1].grid()
+
+f2.set_figheight(5)
+f2.set_figwidth(10)
+
+## Optimized trajectory data ##
+
+f32, ax32 = plt.subplots(5)
 # f1.title()
 ax32[0].plot(time_vect_opt, opt_data["q_p_dot"][0, :-1], drawstyle='steps-post')
 ax32[0].plot(time_vect_opt, opt_data["q_p_dot"][1, :-1], drawstyle='steps-post')
-
-# ax1.plot(selected_time_replay, selected_iq_meas[0, :], drawstyle='steps-post')
-# ax[0].set_xlabel('time',  fontsize=fontsize)
 ax32[0].set_ylabel('[rad/s]', fontsize=fontsize)
 ax32[0].set_title('Joint velocities - opt solution', fontsize=fontsize)
 ax32[0].grid()
@@ -258,8 +299,6 @@ legend.set_draggable(state = True)
 
 ax32[1].plot(time_vect_opt, opt_data["i_q"][0, :], drawstyle='steps-post')
 ax32[1].plot(time_vect_opt, opt_data["i_q"][1, :], drawstyle='steps-post')
-# ax1.plot(selected_time_replay, selected_iq_meas[0, :], drawstyle='steps-post')
-# ax[1].set_xlabel('time',  fontsize=fontsize)
 ax32[1].set_ylabel('[A]', fontsize=fontsize)
 ax32[1].set_title('Quadrature currents - opt solution', fontsize=fontsize)
 ax32[1].grid()
@@ -268,14 +307,32 @@ legend.set_draggable(state = True)
 ax32[2].plot(time_vect_opt, opt_data["tau"][1, :], drawstyle='steps-post')
 ax32[2].plot(time_vect_opt, opt_data["tau"][2, :], drawstyle='steps-post')
 # ax1.plot(selected_time_replay, selected_iq_meas[0, :], drawstyle='steps-post')
-ax32[2].set_xlabel('time [s]',  fontsize=fontsize)
 ax32[2].set_ylabel('[Nm]', fontsize=fontsize)
 ax32[2].set_title('Joint efforts - opt solution', fontsize=fontsize)
 ax32[2].grid()
 legend = ax32[2].legend([r"$\tau_{1}$", r"$\tau_{2}$"], fontsize=fontsize)
 legend.set_draggable(state = True)
-f32.set_figheight(5)
-f32.set_figwidth(10)
+
+ax32[3].plot(time_vect_opt, opt_data["foot_tip_height"][:-1], drawstyle='steps-post')
+ax32[3].plot(time_vect_opt, opt_data["com_pos"][2, :-1], drawstyle='steps-post')
+ax32[3].set_ylabel('[m]', fontsize=fontsize)
+ax32[3].set_title('tip/CoM height - opt solution', fontsize=fontsize)
+ax32[3].grid()
+legend = ax32[3].legend([f"tip", f"CoM"], fontsize=fontsize)
+legend.set_draggable(state = True)
+
+ax32[4].plot(time_vect_opt, opt_data["f_contact"][0, :], drawstyle='steps-post')
+ax32[4].plot(time_vect_opt, opt_data["f_contact"][1, :], drawstyle='steps-post')
+ax32[4].plot(time_vect_opt, opt_data["f_contact"][2, :], drawstyle='steps-post')
+ax32[4].set_xlabel('time [s]',  fontsize=fontsize)
+ax32[4].set_ylabel('[N]', fontsize=fontsize)
+ax32[4].set_title('contact force - opt solution', fontsize=fontsize)
+ax32[4].grid()
+legend = ax32[4].legend([r"$f_0$", r"$f_1$", r"$f_2$"], fontsize=fontsize)
+legend.set_draggable(state = True)
+
+f32.set_figheight(8)
+f32.set_figwidth(13)
 
 
 plt.show()
